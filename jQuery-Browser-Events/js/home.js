a33addc43dde0ef4ef5d9a3a2c865c21b5d5591c
Emma Stout <emma.stout01@gmail.com>
2018-05-15 13:25:39 -0500

    completed jquery exercises

---
+++ jQuery-Browser-Events/js/home.js
@@ -0,0 +1,62 @@
+$(document).ready(onReady());
+
+function onReady() {
+    $('#akronInfoDiv').hide();
+    $('#minneapolisInfoDiv').hide();
+    $('#louisvilleInfoDiv').hide();
+    displayMain();
+    displayMinneapolis();
+    displayAkron();
+    displayLouisville();
+    displayMinneapolisWeather();
+    displayAkronWeather();
+    displayLouisvilleWeather();
+}
+
+function displayMain() {
+    displaySection('main');
+}
+
+function displayMinneapolis() {
+    displaySection('minneapolis');
+}
+
+function displayAkron() {
+    displaySection('akron');
+}
+
+function displayLouisville() {
+    displaySection('louisville');
+}
+
+function displayAkronWeather() {
+    toggleWeatherClick('akron');
+}
+
+function displayMinneapolisWeather() {
+    toggleWeatherClick('minneapolis');
+}
+
+function displayLouisvilleWeather() {
+    toggleWeatherClick('louisville');
+}
+   
+function displaySection(sectionName) {
+    $('#' + sectionName + 'Button').on('click', function() {
+     $('#mainInfoDiv').hide();
+    $('#akronInfoDiv').hide();
+    $('#minneapolisInfoDiv').hide();
+    $('#louisvilleInfoDiv').hide();
+    $('#' + sectionName +'InfoDiv').show();
+   }) 
+}
+
+function toggleWeatherClick(cityName) {
+    var weatherButton = '#' + cityName + 'WeatherButton';
+    var weatherDisplay = '#' + cityName + 'Weather';
+    $(weatherButton).on('click', function() {
+        $(weatherDisplay).toggle();
+        var message = $(weatherDisplay).is(':visible') ? 'Hide Weather' : 'Show Weather';
+        $(weatherButton).text(message);
+    })
+}
