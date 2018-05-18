b2e0307bc54a46fe892623e286be13363569780e
Emma Stout <emma.stout01@gmail.com>
2018-05-18 16:09:56 -0500

    completed basic reqs for vending machine ui

--- VendingMachineUI/controller.js
+++ VendingMachineUI/controller.js
@@ -5,7 +5,7 @@ myApp.controller('controller', function (service) {
     vm.service = service;
     vm.items = [];
     vm.moneyUnfixed = 0;
-    vm.moneyInMachine;
+    vm.moneyInMachine = 0;
     vm.itemId;
     vm.message = "";
     vm.change;
@@ -16,22 +16,20 @@ myApp.controller('controller', function (service) {
             vm.items = response.data;
         })
     }
-
     vm.getTreats();
-
 
     vm.addMoney = function (moneyIn) {
         vm.moneyUnfixed += moneyIn;
         vm.moneyInMachine = vm.moneyUnfixed.toFixed(2);
     }
 
-    vm.getItemId = function(itemId) {
+    vm.getItemId = function (itemId) {
         vm.itemId = itemId;
         console.log(vm.itemId);
     }
 
-    vm.purchaseItem = function(moneyIn, itemId) {
-        vm.service.purchaseItem(moneyIn, itemId).then(function(response) {
+    vm.purchaseItem = function (moneyIn, itemId) {
+        vm.service.purchaseItem(moneyIn, itemId).then(function (response) {
             if (response.data.message != null) {
                 vm.message = response.data.message;
             } else {
@@ -47,6 +45,39 @@ myApp.controller('controller', function (service) {
             vm.itemId = null;
             vm.getTreats();
         })
+    }
+
+    vm.returnChange = function () {
+        var amountToSubtract = 0;
+        var amountInPennies = vm.moneyInMachine * 100;
+        var quarters = 0;
+        var dimes = 0;
+        var nickels = 0;
+        var pennies = 0;
+
+        quarters = Math.floor(amountInPennies / 25);
+        amountToSubtract = quarters * 25;
+        amountInPennies = amountInPennies - amountToSubtract;
+
+        dimes = Math.floor(amountInPennies / 10);
+        amountToSubtract = dimes * 10;
+        amountInPennies = amountInPennies - amountToSubtract;
+
+        nickels = Math.floor(amountInPennies / 5);
+        amountToSubtract = nickels * 5;
+        amountInPennies = amountInPennies - amountToSubtract;
+
+        vm.change = {
+            quarters: quarters,
+            dimes: dimes,
+            nickels: nickels,
+            pennies: pennies
+        }
+
+        vm.message = "";
+        vm.itemId = null;
+        vm.moneyInMachine = 0;
+        vm.moneyUnfixed = 0;
     }
 
 
