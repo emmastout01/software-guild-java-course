f06b46a332bf4982b376446b0a7fda5782c0f35b
Emma Stout <emma.stout01@gmail.com>
2018-05-23 11:25:33 -0500

    minor changes to holiday create and read

--- JewishHolidaysList/src/main/java/com/sg/jewishholidayslist/controllers/MainController.java
+++ JewishHolidaysList/src/main/java/com/sg/jewishholidayslist/controllers/MainController.java
@@ -24,7 +24,8 @@ public class MainController {
     
     private static final List<Holiday> holidays = new ArrayList<>();
     
-    @GetMapping("/holidays")
+    @GetMapping
+      //Here, we return a string because we're returning the (string) name of a template
     public String getHolidays(Model model) {
         model.addAttribute("holidays", holidays);
         return "holidays";
@@ -32,12 +33,14 @@ public class MainController {
     
     
     
-    @PostMapping("/addHoliday")
-    public String addHoliday(Model model, String holidayName, String startDate, String endDate, String description) {
-        Holiday holiday = new Holiday(holidayName, startDate, endDate, description);
+    @PostMapping("/addHoliday") 
+    //Here, we return a string because we're returning the (string) name of a route
+    public String addHoliday(Holiday holiday) {
+//        Model model, String holidayName, String startDate, String endDate, String description
+//        Holiday holiday = new Holiday(holidayName, startDate, endDate, description);
         holidays.add(holiday);
         System.out.println("Holiday: " + holiday);
         System.out.println("Holiday list: " + holidays);
-        return "redirect:/holidays";
+        return "redirect:/";
     }
 }
