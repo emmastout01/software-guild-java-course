edf53c5eec086fc3263464311f5915c1f7017f6d
Emma Stout <emma.stout01@gmail.com>
2018-06-04 12:02:11 -0500

    contact form created

--- car-dealership-client/src/pages/AllUsers/Contact.js
+++ car-dealership-client/src/pages/AllUsers/Contact.js
@@ -10,40 +10,26 @@ class Contact extends Component {
                 name: '',
                 email: '',
                 phone: '',
-                message: '',
+                message: props.match.params.vin,
             }
         }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
 
-    // componentDidMount() {
-    //     const { match: { params } } = this.props;
+    componentDidMount() {
+        const { match: { params } } = this.props;
+        const message = this.state.message;
+      
 
-    //     //Write a POST route for a contact message
+    }
 
-    //     axios.post('http://localhost:8080/message', {
-    //         name:
-    //         email: 
-    //         phone:
-    //         message:   
-
-    //     }, config)
-
-    // }
-
-    // handleInputChange(event) {
-    //     this.setState({
-    //         message: {
-    //             ...this.state.message,
-
-    //         }
-    //     })
-    // }
+ 
 
     //Currying: Returns a function
     handleChangeFor = (propertyName) => {
         return (event) => {
-            console.log(event.target.value);
             this.setState({
                 message: {
                     ...this.state.message,
@@ -52,6 +38,37 @@ class Contact extends Component {
                 }
             })
         }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.message);
+
+          //Write a POST route for a contact message
+
+        // axios.post('http://localhost:8080/message', {
+        //     name: message.name,
+        //     email: message.email,
+        //     phone: message.phone,
+        //     message: message.message,
+
+    //     // }, config)
+    // })
+        console.log('form submitted: ', this.state.message);
+        this.emptyState();
+      
+
+    }
+
+    emptyState() {
+        this.setState({
+            message: {
+                name: '',
+                email: '',
+                phone: '',
+                message: this.props.match.params.vin
+            }
+        })
     }
 
     render() {
@@ -68,15 +85,17 @@ class Contact extends Component {
         else {
             return (
                 <div>
+                    <form onSubmit={this.handleSubmit}>
                     <h2>Contact with vin</h2>
-                    Name: <input onChange={this.handleChangeFor('name')} />
+                    Name: <input value={this.state.message.name} onChange={this.handleChangeFor('name')} />
                     <br />
-                    Email: <input onChange={this.handleChangeFor('email')} />
+                    Email: <input value={this.state.message.email} onChange={this.handleChangeFor('email')} />
                     <br />
-                    Phone: <input onChange={this.handleChangeFor('phone')} />
+                    Phone: <input value={this.state.message.phone} onChange={this.handleChangeFor('phone')} />
                     <br />
-                    Message: <textarea onChange={this.handleChangeFor('name')} defaultValue={params.vin} />
-                    
+                    Message: <textarea value={this.state.message.message} onChange={this.handleChangeFor('message')} default={params.vin} />
+                    <input type='submit' value='Submit' />
+                    </form >
             </div>
             )
         }
